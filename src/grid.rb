b0b3bdd92e67098266d71bdbda79ee97572ebbea
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-03 23:54:41 +1000

    working maze printer with walls stored as 2d array

--- src/grid.rb
+++ src/grid.rb
@@ -1,21 +1,45 @@
 # Print grid to screen
+require_relative 'cell'
 
 # Grid size
 width = 10
 height = 10
 
-# puts top_border
-# for row_num in 0...height
-#   puts '|' + vertical_walls(row_num) + '|'
-#   puts '+' + horizontal_walls(row_num) + '+'
-# end
-cells_2d_array = Array.new(width) { Array.new(height) }
-cells_2d_array.map! { |c| c = '-' }
-cells_2d_array.inspect
+top_border = '+   +' + '---+' * (width - 1)
+# Create 2d array of all horizontal walls and set to true
+horiz_walls = Array.new(width) { Array.new(height) { true } }
+# Same for vertical walls...
+vert_walls = Array.new(width) { Array.new(height) { true } }
+# Remove finish wall
+horiz_walls[-1][-1] = false
+# Remove walls for testing
+vert_walls[5][7] = false
+horiz_walls[7][1] = false
 
-horizontal_border = "+---" * width + '+'
-puts horizontal_border
-for row_num in 0...height
-  puts "|   " * width + '|'
-  puts "+---" * width + '+'
+player = { x: 0, y: 0 }
+
+puts top_border
+for row in 0...(height)
+  # Print vertical walls
+  print '|' # Leftside border
+  for col in 0...(width)
+    # Add player symbol
+    if col == player[:x] && row == player[:y]
+      print vert_walls[row][col] ? ' * |' : ' *  '
+    else
+      print vert_walls[row][col] ? '   |' : '    '
+    end
+  end
+  print "\n+" # Rightside border
+  # Print horizontal walls
+  for col in 0...(width)
+    print horiz_walls[row][col] ? '---+' : '   +'
+  end
+  print "\n"
 end
+
+# cells_2d_array = Array.new(width - 1) { Array.new(height - 1) }
+
+# cells_2d_array.each do |row_arr|
+#   row_arr.map! { |c| c = Cell.new }
+# end
