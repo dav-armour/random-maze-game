d6d5193b41d50cccf7c56c1bc12955c8cfc9203e
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-04 09:40:32 +1000

    move to maze class file, create game.rb

---
+++ src/models/maze.rb
@@ -0,0 +1,53 @@
+# Print grid to screen
+require_relative 'cell'
+class Maze
+  attr_accessor :width, :height
+  def initialize (width = 10, height = 10)
+    @width = 10
+    @height = 10
+    @player = { x: 8, y: 2 }
+  end
+
+  def create_walls
+    # Create 2d array of all horizontal walls and set all to true
+    @horiz_walls = Array.new(@width) { Array.new(@height) { true } }
+    # Same for vertical walls...
+    @vert_walls = Array.new(@width) { Array.new(@height) { true } }
+    # Remove finish wall
+    @horiz_walls[-1][-1] = false
+    # Remove walls for testing
+    @vert_walls[5][7] = false
+    @horiz_walls[7][1] = false
+  end
+
+  def maze_to_s
+    # Top Border
+    output = '+   +' + '---+' * (@width - 1) + "\n"
+    for row in 0...@height
+      # Print vertical walls
+      output += '|' # Leftside border
+      for col in 0...@width
+        # Add player symbol
+        if col == player[:x] && row == player[:y]
+          output += @vert_walls[row][col] ? ' * |' : ' *  '
+        else
+          output += @vert_walls[row][col] ? '   |' : '    '
+        end
+      end
+      output += "\n+" # Rightside border
+      # Print horizontal walls
+      for col in 0...@width
+        output += @horiz_walls[row][col] ? '---+' : '   +'
+      end
+      output += "\n"
+    end
+    output
+  end
+
+end
+
+# cells_2d_array = Array.new(width - 1) { Array.new(@height - 1) }
+
+# cells_2d_array.each do |row_arr|
+#   row_arr.map! { |c| c = Cell.new }
+# end
