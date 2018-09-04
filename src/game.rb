20dde55c298d044c1f2a37bd078b0dd6598f1811
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-04 10:25:36 +1000

    create game loop

--- src/game.rb
+++ src/game.rb
@@ -4,5 +4,12 @@ require_relative 'models/maze'
 
 class Game
   maze = Maze.new
-  puts maze.to_s
+  while true
+    puts maze.to_s
+    # FIXME: get input from user to move player
+    input = 'q'
+    if input == 'q'
+      break
+    end
+  end
 end
--- src/models/cell.rb
+++ src/models/cell.rb
@@ -1,4 +1,7 @@
 class Cell
   def initialize
+    @available_directions = []
+    # Keep track for maze creation
+    @visited = false
   end
 end
