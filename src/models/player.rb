34e9d900180a81ac89adbeab644a5dad436abef2
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-04 17:19:44 +1000

    make player unable to move through walls
    Co-authored-by: Tyson <technics652@gmail.com>

--- src/models/player.rb
+++ src/models/player.rb
@@ -7,18 +7,26 @@ class Player
   end
 
   def move_north(maze)
-    @ypos -= 1 unless @ypos == 0
+    if maze.cells[xpos][ypos].available_directions.include?('N')
+      @ypos -= 1 unless @ypos == 0
+    end
   end
 
   def move_south(maze)
-    @ypos += 1 unless @ypos == maze.height - 1 #Fixme
+    if maze.cells[xpos][ypos].available_directions.include?('S')
+      @ypos += 1 unless @ypos == maze.height - 1 #Fixme
+    end
   end
 
   def move_east(maze)
-    @xpos += 1 unless @xpos == maze.width - 1 #Fixme
+    if maze.cells[xpos][ypos].available_directions.include?('E')
+      @xpos += 1 unless @xpos == maze.width - 1 #Fixme
+    end
   end
 
   def move_west(maze)
-    @xpos -= 1 unless @xpos == 0 #Fixme
+    if maze.cells[xpos][ypos].available_directions.include?('W')
+      @xpos -= 1 unless @xpos == 0 #Fixme
+    end
   end
 end
