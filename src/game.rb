e0daab9e060f2337099d8fbd8a6d7de258e96f06
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-04 14:04:17 +1000

    give player ability to move using arrow keys
    Co-authored-by: Tyson <technics652@gmail.com>

--- src/game.rb
+++ src/game.rb
@@ -1,4 +1,5 @@
 # Main game file
+require 'io/console'
 require_relative 'models/player'
 require_relative 'models/maze'
 
@@ -6,12 +7,10 @@ class Game
   def run
     @maze = Maze.new
     @player = Player.new
-    exit = false
     while true
       system 'clear'
       puts @maze.to_s(@player)
       # FIXME: get input from user to move player
-      exit = false
       exit = get_input
       break if exit
       # puts "Current position X: #{player.xpos}, Y: #{player.ypos}"
@@ -19,22 +18,32 @@ class Game
   end
 
   def get_input()
-    input = gets.chomp
-    case input
-    when 'w'
+    char = STDIN.getch
+    # Check if special character entered (arrow keys)
+    if char == "\e"
+      # Retrieve rest of special character
+      2.times do
+        char += STDIN.getch
+      end
+    end
+
+    case char
+    when 'w', "\e[A"
       @player.move_north(@maze)
       false
-    when 's'
+    when 's', "\e[B"
       @player.move_south(@maze)
       false
-    when 'd'
+    when 'd', "\e[C"
       @player.move_east(@maze)
       false
-    when 'a'
+    when 'a', "\e[D"
       @player.move_west(@maze)
       false
     when 'q'
       true
+    when "\u0003"
+      exit
     else
       false
     end
