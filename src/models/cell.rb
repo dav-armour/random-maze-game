5ca4405be23f294a4ebb289f97080056a6f2a48a
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-04 14:18:38 +1000

    add cell class
    Co-authored-by: Tyson <technics652@gmail.com>

--- src/models/cell.rb
+++ src/models/cell.rb
@@ -1,6 +1,11 @@
 class Cell
-  def initialize
+  attr_accessor :available_directions, :visited, :x, :y
+  def initialize x, y
+    # Available movement directions N,S,E,W
     @available_directions = []
+    # Position in maze
+    @x = x
+    @y = y
     # Keep track for maze creation
     @visited = false
   end
