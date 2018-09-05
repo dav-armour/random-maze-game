af64f4d346a3a7dc3dc4fa2b4dc88741ad145dfa
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-05 11:07:23 +1000

    add basic scoring based on time
    Co-authored-by: Tyson <technics652@gmail.com>

--- src/game.rb
+++ src/game.rb
@@ -4,17 +4,45 @@ require_relative 'models/player'
 require_relative 'models/maze'
 
 class Game
-  def run
-    @maze = Maze.new(20,10)
+  def run(difficulty = :easy)
+    @maze = Maze.new
     @player = Player.new
+    start_time = Time.now.to_i
     while true
       system 'clear'
+      position = [@player.x, @player.y]
+      end_x = @maze.width - 1
+      end_y = @maze.height - 1
+      # Check if they have reached the finish
+      if position == [end_x, end_y]
+        break
+      end
       puts @maze.to_s(@player)
       # FIXME: get input from user to move player
       exit = get_input
+
       break if exit
       # puts "Current position X: #{player.xpos}, Y: #{player.ypos}"
     end
+    end_time = Time.now.to_i
+    time_taken = end_time - start_time
+    score = get_score(time_taken, difficulty)
+    puts "You made it throught the maze in #{time_taken} seconds!"
+    puts "Your time bonus is #{score}!"
+  end
+
+  def get_score(time_taken, difficulty)
+    #Calculate score
+    case difficulty
+    when :easy
+      time_differnce =  15 - time_taken
+    when :medium
+      time_differnce =  15 - time_taken
+    when :hard
+      time_differnce =  20 - time_taken
+    end
+    time_bonus = time_differnce <= 0 ? 0 : time_differnce * 50
+    time_bonus
   end
 
   def get_input()
