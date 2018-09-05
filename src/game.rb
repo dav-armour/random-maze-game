eba480b185bb2981925b48ad7fb550f3ddfeeb15
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-05 12:11:15 +1000

    create menus and print maze inside
    Co-authored-by: Tyson <technics652@gmail.com>

--- src/game.rb
+++ src/game.rb
@@ -1,9 +1,22 @@
 # Main game file
 require 'io/console'
+require 'rainbow'
 require_relative 'models/player'
 require_relative 'models/maze'
+require_relative 'models/menu'
+
 
 class Game
+
+  def initialize
+    @menus = {}
+    system 'clear'
+    create_menus
+    # @menus[:difficulty_menu].display_menu
+    # @menus[:result_menu].display_menu
+    # @menus[:maze_menu].display_menu
+  end
+
   def run(difficulty = :easy)
     @maze = Maze.new
     @player = Player.new
@@ -17,18 +30,31 @@ class Game
       if position == [end_x, end_y]
         break
       end
-      puts @maze.to_s(@player)
-      # FIXME: get input from user to move player
-      exit = get_input
+      # p @menus[:maze_menu]
+      @menus[:maze_header].display_menu
+      print_maze(@player)
+      # puts @maze.to_s(@player).center(100)
+      @menus[:maze_footer].display_menu
 
-      break if exit
+      menu = get_input
+
+      break if menu
       # puts "Current position X: #{player.xpos}, Y: #{player.ypos}"
     end
     end_time = Time.now.to_i
     time_taken = end_time - start_time
     score = get_score(time_taken, difficulty)
-    puts "You made it throught the maze in #{time_taken} seconds!"
     puts "Your time bonus is #{score}!"
+  end
+
+  def print_maze(player)
+    puts Rainbow('|' + ' '.center(98) + '|').green
+    @maze.to_s(player).split("\n").each do |line|
+      print Rainbow('|').green
+      print line.center(98)
+      print Rainbow("|\n").green
+    end
+    puts Rainbow('|' + ' '.center(98) + '|').green
   end
 
   def get_score(time_taken, difficulty)
@@ -56,7 +82,7 @@ class Game
     end
 
     case char
-    # Makes wasd and arrow keys move player
+      # Makes wasd and arrow keys move player
     when 'w', "\e[A"
       @player.move_north(@maze)
       false
@@ -69,16 +95,54 @@ class Game
     when 'a', "\e[D"
       @player.move_west(@maze)
       false
-    # Exit game
-    when 'q'
+      # Exit game
+    when 'm'
       true
-    # Ctrl - C to exit game
-    when "\u0003"
+      # Ctrl - C to exit game
+    when "\u0003", "q"
       exit
     else
       false
     end
   end
+
+  def create_menus
+    # Difficulty Menu
+    header_text = 'Random Maze Game'
+    header = { text: header_text, color: :red }
+    body_text = "Welcome to our super awesome maze game\n-\nPlease choose a difficulty:\n"
+    body_choices = ['Easy', 'Medium', 'Hard']
+    body = {text: body_text, choices: body_choices, color: :white }
+    footer_text = "Press Q or Ctrl-C to quit"
+    footer = { text: footer_text, align: 'rjust', color: :blue }
+    difficulty_menu = Menu.new(width: 100, header: header, body: body, footer: footer)
+    difficulty_menu.border_color = :green
+    @menus[:difficulty_menu] = difficulty_menu
+
+    #Results Menu
+    body_text = "Congratulations you finished the maze.\n-\n"
+    body_text += "You made it throught the maze in 5 seconds!\n-\n"
+    body_text += "Your score is 300!\n-"
+    body_text += "\nPlease choose an option:\n"
+    body_choices = ['Restart Maze', 'Change Difficulty', 'Exit']
+    body = {text: body_text, choices: body_choices, color: :white }
+    # body_text = "Congratulations you finished the maze.\n"
+    # body_text += "You made it throught the maze in #{time_taken} seconds!\n"
+    # body_text += "Your score is #{score}!"
+    result_menu = Menu.new(width: 100, header: header, body: body, footer: footer)
+    result_menu.border_color = :green
+    @menus[:result_menu] = result_menu
+
+    # Maze header
+    maze_header = Menu.new(width: 100, header: header)
+    maze_header.border_color = :green
+    @menus[:maze_header] = maze_header
+    # Maze footer
+    maze_footer = Menu.new(width: 100, footer: footer)
+    maze_footer.border_color = :green
+    @menus[:maze_footer] = maze_footer
+  end
+
 end
 
 Game.new.run
